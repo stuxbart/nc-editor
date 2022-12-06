0b4ac1cdef30634d356f6bf7f2af0dee4035545a
stuxbart <stuxbart99@gmail.com>
2022-12-06 19:14:04 +0100

    feat: added themes list for view

---
+++ src/ui/themes.ts
@@ -0,0 +1,8 @@
+export const VALID_THEMES = ['default', 'light', 'dark'];
+export const BASE_THEME_CLASS = 'nc-theme--';
+export enum Theme {
+	Default = 'default',
+	Light = 'light',
+	Dark = 'dark',
+}
+export const DEFAULT_THEME = 'default';
