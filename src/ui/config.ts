a01dbad4e43a749d19c48edb5eafe75af2b6e7e7
stuxbart <stuxbart99@gmail.com>
2022-12-06 20:11:20 +0100

    feat: added ui config file

---
+++ src/ui/config.ts
@@ -0,0 +1,10 @@
+export const EDITOR_INPUT_CLASS = 'nc-editor__input';
+export const EDITOR_INPUT_ID = 'editor-input';
+export const EDITOR_FONT_FAMILY =
+	'ui-monospace,SFMono-Regular,SF Mono,Menlo,Consolas,Liberation Mono,monospace';
+export const EDITOR_CURSOR_CSS_CLASS = 'nc-selection__cursor';
+export const EDITOR_CURSOR_ANIMATED_CSS_CLASS = 'nc-selection__cursor--animated';
+export const EDITOR_MULTI_LINE_CSS_CLASS = 'nc-text__multi-line';
+export const EDITOR_MULTI_LINE_ACTIVE_CSS_CLASS = 'nc-text__multi-line--active';
+export const EDITOR_MULTI_LINE_HOVERED_CSS_CLASS = 'nc-text__multi-line--hovered';
+export const EDITOR_MULTI_LINE_CONTENT_CSS_CLASS = 'nc-text__multi-line_content';
