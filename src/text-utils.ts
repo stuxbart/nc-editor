3105558190f29ba65eb88d1cd8395d47251a69e8
stuxbart <stuxbart99@gmail.com>
2022-12-06 21:19:10 +0100

    feat: added text utility functions

--- src/text-utils.ts
+++ src/text-utils.ts
@@ -4,3 +4,34 @@ export function removeAccents(text: string): string {
 		.replace(/[\u0300-\u036f]/g, '') // get rid of accents
 		.replace('ł', 'l'); // doesnt work for ł
 }
+
+export function columnToOffset(line: string, column: number, tabSize: number = 8): number {
+	let offset: number = 0;
+	while (offset < line.length && column > 0) {
+		if (line[offset++] === '\t') {
+			column -= tabSize;
+		} else {
+			column -= 1;
+		}
+	}
+	if (column > 0) {
+		return line.length;
+	}
+	if (column < -tabSize / 2) {
+		return offset - 1;
+	}
+	return offset;
+}
+
+export function offsetToColumn(line: string, offset: number, tabSize: number = 8): number {
+	let column: number = 0;
+	for (let i = 0; i < offset; i++) {
+		const char = line[i];
+		if (char === '\t') {
+			column += tabSize;
+		} else {
+			column += 1;
+		}
+	}
+	return column;
+}
