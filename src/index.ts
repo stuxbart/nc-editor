5d3863d20292e76dfd01e27d02060b72b563d174
stuxbart <stuxbart99@gmail.com>
2022-12-06 22:00:02 +0100

    feat: added styles

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { Document } from './document';
 import { Range } from './selection';
 import { Editor } from './editor';
 import { EditorView } from './ui';
+import './styles/main.scss';
 
 export { Document, Range, Editor, EditorView };
