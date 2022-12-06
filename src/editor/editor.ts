d0c3cf6f487738188e3245459c7903aa49e2bf31
stuxbart <stuxbart99@gmail.com>
2022-12-06 18:37:40 +0100

    feat: update Editor to use Tokenizer

--- src/editor/editor.ts
+++ src/editor/editor.ts
@@ -1,5 +1,6 @@
 import Document from '../document/document';
 import { Selection } from '../selection';
+import { Tokenizer } from '../tokenizer';
 
 /**
  * Editor class manages state of editor.
@@ -7,18 +8,22 @@ import { Selection } from '../selection';
 class Editor {
 	private _selections: Selection[] = [];
 	private _document: Document | null = null;
+	private _tokenizeAfterEdit: boolean = false;
+	private _tokenizer: Tokenizer;
 
 	constructor() {
 		const newDoc = new Document('');
 		this._document = newDoc;
 		this._selections = [new Selection(0, 0, 0, 0)];
+		this._tokenizer = new Tokenizer(this._document, false);
 	}
 
 	public insert(str: string, line: number, offset: number): void {
 		if (this._document === null) {
 			return;
 		}
-		this._document.insert(str, line, offset);
+		const insertedLines = this._document.insert(str, line, offset);
+		this._updateLinesTokens(line, insertedLines[0] + 1);
 	}
 
 	public remove(type: number = 0): void {
@@ -33,15 +38,31 @@ class Editor {
 				sel.start.offset = sel.start.offset - 1;
 			}
 		}
+		this._updateLinesTokens(sel.start.line, 2);
 		this._document.remove(sel);
 	}
 
 	public setDocument(document: Document): void {
 		this._document = document;
+		this.tokenize();
 	}
 
 	public getText(): string {
 		return this._document?.getText() ?? '';
+	}
+
+	public tokenize(): void {
+		if (this._document === null) {
+			return;
+		}
+		this._tokenizer.setDocument(this._document);
+		this._tokenizer.makeTokens();
+	}
+
+	private _updateLinesTokens(firstLine: number, linesCount: number): void {
+		if (this._tokenizeAfterEdit) {
+			this._tokenizer.updateLinesTokens(firstLine, linesCount);
+		}
 	}
 }
 
