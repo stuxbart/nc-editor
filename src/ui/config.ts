2a2a257accab841ab5eba5220a303db245eab359
stuxbart <stuxbart99@gmail.com>
2022-12-07 12:59:46 +0100

    fix: added missing css classes to config file

--- src/ui/config.ts
+++ src/ui/config.ts
@@ -8,5 +8,7 @@ export const EDITOR_MULTI_LINE_CSS_CLASS = 'nc-text__multi-line';
 export const EDITOR_MULTI_LINE_ACTIVE_CSS_CLASS = 'nc-text__multi-line--active';
 export const EDITOR_MULTI_LINE_HOVERED_CSS_CLASS = 'nc-text__multi-line--hovered';
 export const EDITOR_MULTI_LINE_CONTENT_CSS_CLASS = 'nc-text__multi-line_content';
+export const EDITOR_SELECTION_RANGE_CSS_CLASS = 'nc-selection__range';
+export const EDITOR_SELECTION_CSS_CLASS = 'nc-editor__selection';
 
 export const MAX_LINES_COUNT_ON_DEFAULT_SCROLL_SCALE = 500000;
