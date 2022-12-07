d0464d52f575ad481f2ddac9c324f62b97ded3e5
stuxbart <stuxbart99@gmail.com>
2022-12-07 13:02:44 +0100

    feat: added method to update selection

--- src/selection/selection.ts
+++ src/selection/selection.ts
@@ -1,5 +1,6 @@
+import Point from './point';
 import Range from './range';
-import { samePoint } from './utils';
+import { pointCompare, samePoint } from './utils';
 
 export enum SelectionType {
 	N,
@@ -12,5 +13,40 @@ export default class Selection extends Range {
 
 	public get isCollapsed(): boolean {
 		return samePoint(this.start, this.end);
+	}
+
+	public updateSelection(point: Point): void {
+		if (this.type === SelectionType.L) {
+			const cmpRes = pointCompare(this.end, point);
+			if (cmpRes === 1) {
+				this.start = point;
+			} else {
+				this.type = SelectionType.R;
+				this.start = this.end;
+				this.end = point;
+			}
+		} else if (this.type === SelectionType.R) {
+			const cmpRes = pointCompare(this.start, point);
+			if (cmpRes === 2) {
+				this.end = point;
+			} else {
+				this.type = SelectionType.L;
+				this.end = this.start;
+				this.start = point;
+			}
+		} else {
+			const cmpRes = pointCompare(this.start, point);
+			if (cmpRes === 2) {
+				this.type = SelectionType.R;
+				this.end = point;
+			} else {
+				this.type = SelectionType.L;
+				this.start = point;
+			}
+		}
+	}
+
+	public toString(): string {
+		return `{start: {l: ${this.start.line}, o:${this.start.offset}}, end: {l: ${this.end.line}, o:${this.end.offset}}}`;
 	}
 }
