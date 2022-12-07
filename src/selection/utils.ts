6f54c560739495a4b57dba3827a5c9d5f2682b6d
stuxbart <stuxbart99@gmail.com>
2022-12-07 13:00:12 +0100

    feat: added way to compare Point objects

--- src/selection/utils.ts
+++ src/selection/utils.ts
@@ -2,3 +2,24 @@ import Point from './point';
 
 export const samePoint = (p1: Point, p2: Point): boolean =>
 	p1.line === p2.line && p1.offset === p2.offset;
+
+/**
+ * Return:
+ * 0 if points are equal
+ * 1 if first point is greater
+ * 2 if second point is greater
+ * greater - later in text
+ */
+export const pointCompare = (p1: Point, p2: Point): number => {
+	if (p1.line === p2.line) {
+		if (p1.offset < p2.offset) {
+			return 2;
+		} else if (p1.offset === p2.offset) {
+			return 0;
+		}
+		return 1;
+	} else if (p1.line < p2.line) {
+		return 2;
+	}
+	return 1;
+};
