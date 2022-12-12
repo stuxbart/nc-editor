9cea035f5eccfc7f1112a0255526f654d9d03bcb
stuxbart <stuxbart99@gmail.com>
2022-12-12 11:12:27 +0100

    feat: added randomString utility function

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,12 @@
 export function notEmpty<TValue>(value: TValue | null | undefined): value is TValue {
 	return value !== null && value !== undefined;
 }
+
+export function randomString(length: number = 6): string {
+	let id = '';
+	const availableChars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	for (let i = 0; i < length; i++) {
+		id += availableChars[Math.floor(Math.random() * availableChars.length)];
+	}
+	return id;
+}
