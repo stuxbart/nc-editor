e9c09d3b2afb18ca3c59bfe80922b17a5345a83e
stuxbart <stuxbart99@gmail.com>
2022-12-20 14:34:07 +0100

    feat: generate unique id

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+const IDS_REGISTRY: string[] = [];
+
 export function notEmpty<TValue>(value: TValue | null | undefined): value is TValue {
 	return value !== null && value !== undefined;
 }
@@ -8,5 +10,14 @@ export function randomString(length: number = 6): string {
 	for (let i = 0; i < length; i++) {
 		id += availableChars[Math.floor(Math.random() * availableChars.length)];
 	}
+	return id;
+}
+
+export function randomId(length: number = 6): string {
+	let id = '';
+	do {
+		id = randomString(length);
+	} while (IDS_REGISTRY.includes(id));
+	IDS_REGISTRY.push(id);
 	return id;
 }
