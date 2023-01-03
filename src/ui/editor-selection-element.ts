4e7e389b857d5476d6d566fab6f1f1699f0fca94
stuxbart <stuxbart99@gmail.com>
2023-01-03 20:47:17 +0100

    refactor: selection rendering

--- src/ui/editor-selection-element.ts
+++ src/ui/editor-selection-element.ts
@@ -16,59 +16,64 @@ export default class EditorSelectionElement {
 	}
 
 	public render(rows: Row[], letterWidth: number): string {
-		if (this._selection.isCollapsed) {
+		const sel = this._selection;
+		if (sel.isCollapsed || rows.length === 0) {
 			return '';
 		}
-		const sel = this._selection;
+		const firstRow = rows[0];
+		const lastRow = rows[rows.length - 1];
+		if (firstRow.line > sel.end.line) {
+			return '';
+		}
+
+		if (lastRow.line < sel.start.line) {
+			return '';
+		}
+
+		if (firstRow.line === sel.end.line && firstRow.offset > sel.end.offset) {
+			return '';
+		}
+
+		if (
+			lastRow.line === sel.start.line &&
+			lastRow.offset + lastRow.text.length < sel.start.offset
+		) {
+			return '';
+		}
 
 		let firstRowNumber = 0;
 		let firstRowOffset = 0;
-		if (
-			!(sel.start.line < rows[0].line) &&
-			!(sel.start.line === rows[0].line && sel.start.offset <= rows[0].offset)
-		) {
-			for (firstRowNumber = 0; firstRowNumber < rows.length; firstRowNumber++) {
-				const row = rows[firstRowNumber];
-				if (
-					row.line === sel.start.line &&
-					row.offset + row.text.length >= sel.start.offset &&
-					sel.start.offset >= row.offset
-				) {
-					firstRowOffset = sel.start.offset - row.offset;
+
+		if (sel.start.line >= firstRow.line) {
+			for (let i = 0; i < rows.length; i++) {
+				const row = rows[i];
+				if (row.line !== sel.start.line) {
+					continue;
+				}
+				if (row.offset + row.text.length >= sel.start.offset) {
+					firstRowNumber = i;
+					firstRowOffset = sel.start.offset - rows[i].offset;
 					break;
 				}
 			}
 		}
-		if (firstRowNumber === rows.length) {
-			return '';
-		}
-		let lastRowNumber = 0;
-		let lastRowOffset = 0;
-		if (
-			!(sel.end.line < rows[0].line) &&
-			!(sel.end.line === rows[0].line && sel.end.offset <= rows[0].offset)
-		) {
-			const lastRow = rows[rows.length - 1];
-			if (lastRow.line < sel.end.line) {
-				lastRowNumber = rows.length - 1;
-				lastRowOffset = rows[lastRowNumber].text.length;
-			} else {
-				for (lastRowNumber = 0; lastRowNumber < rows.length; lastRowNumber++) {
-					const row = rows[lastRowNumber];
-					if (
-						row.line === sel.end.line &&
-						row.offset + row.text.length >= sel.end.offset &&
-						sel.end.offset >= row.offset
-					) {
-						lastRowOffset = sel.end.offset - row.offset;
-						break;
-					}
+
+		let lastRowNumber = rows.length - 1;
+		let lastRowOffset = lastRow.text.length;
+
+		if (sel.end.line <= lastRow.line) {
+			for (let i = firstRowNumber; i < rows.length; i++) {
+				const row = rows[i];
+				if (row.line !== sel.end.line) {
+					continue;
+				}
+				if (row.offset + row.text.length >= sel.end.offset) {
+					lastRowNumber = i;
+					lastRowOffset = sel.end.offset - row.offset;
+					break;
 				}
 			}
-		} else {
-			return '';
 		}
-
 		const rowsLengths = rows.map((row) => row.text.length);
 		let html = '';
 		for (let i = firstRowNumber; i < lastRowNumber + 1; i++) {
@@ -87,6 +92,7 @@ export default class EditorSelectionElement {
 			const height = px(20);
 			html += `<div class="${CSSClasses.SELECTION_RANGE}" style="top: ${top}; left: ${left}; width: ${width}; height: ${height}"></div>`;
 		}
+
 		return html;
 	}
 }
