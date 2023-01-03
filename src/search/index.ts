8ccd7e63f663a5ca5c9ca67be024e84e7c7a4b12
stuxbart <stuxbart99@gmail.com>
2023-01-03 18:44:12 +0100

    feat: added regexp search

--- src/search/index.ts
+++ src/search/index.ts
@@ -1,5 +1,6 @@
 import SerachResults from './search-results';
 import { Search } from './search';
 import NaiveSearch from './naive-search';
+import RegExpSearch from './regex-search';
 
-export { Search, SerachResults, NaiveSearch };
+export { Search, SerachResults, NaiveSearch, RegExpSearch };
---
+++ src/search/regex-search.ts
@@ -0,0 +1,122 @@
+import { Document } from '../document';
+import { Range } from '../selection';
+import { Search } from './search';
+import SearchResults from './search-results';
+
+export default class RegExpSearch extends Search {
+	public search(phrase: string, document: Document, searchResults: SearchResults): void {
+		searchResults.clearResults();
+		searchResults.phrase = phrase;
+		if (phrase.length === 0) {
+			return;
+		}
+		const regexp = new RegExp(phrase, 'g');
+
+		for (let i = 0; i < document.linesCount; i++) {
+			const line = document.getLine(i);
+			const lineResults = this._searchInLine(regexp, line, i);
+			searchResults.addResults(lineResults);
+		}
+	}
+
+	public searchInLines(
+		phrase: string,
+		document: Document,
+		searchResults: SearchResults,
+		lines: Set<number>,
+	): void {
+		searchResults.clearResults();
+		searchResults.phrase = phrase;
+		if (phrase.length === 0) {
+			return;
+		}
+		const regexp = new RegExp(phrase, 'g');
+
+		for (let i = 0; i < document.linesCount; i++) {
+			if (!lines.has(i)) {
+				continue;
+			}
+			const line = document.getLine(i);
+			const lineResults = this._searchInLine(regexp, line, i);
+			searchResults.addResults(lineResults);
+		}
+	}
+
+	public updateLineSearchResults(
+		document: Document,
+		searchResults: SearchResults,
+		lineNumber: number,
+	): void {
+		if (searchResults.phrase.length === 0) {
+			return;
+		}
+		let line: string = '';
+		try {
+			line = document.getLine(lineNumber);
+		} catch (err) {
+			return;
+		}
+		const regexp = new RegExp(searchResults.phrase, 'g');
+		searchResults.clearLineResults(lineNumber);
+		const lineResults = this._searchInLine(regexp, line, lineNumber);
+		searchResults.addResults(lineResults);
+	}
+
+	public updateLinesSearchResults(
+		document: Document,
+		searchResults: SearchResults,
+		firstLineNumber: number,
+		linesCount: number,
+	): void {
+		if (searchResults.phrase.length === 0) {
+			return;
+		}
+		const regexp = new RegExp(searchResults.phrase, 'g');
+		for (let i = 0; i < linesCount; i++) {
+			const lineNumber = firstLineNumber + i;
+			let line: string;
+			try {
+				line = document.getLine(lineNumber);
+			} catch (err) {
+				return;
+			}
+			searchResults.clearLineResults(lineNumber);
+			const lineResults = this._searchInLine(regexp, line, lineNumber);
+			searchResults.addResults(lineResults);
+		}
+	}
+
+	public updateNewLinesSearchResults(
+		document: Document,
+		searchResults: SearchResults,
+		firstLineNumber: number,
+		newLinesCount: number,
+	): void {
+		searchResults.applyLinesDelta(firstLineNumber, newLinesCount);
+		this.updateLinesSearchResults(document, searchResults, firstLineNumber, newLinesCount);
+	}
+
+	public updateRemovedLinesSearchResults(
+		document: Document,
+		searchResults: SearchResults,
+		firstLineNumber: number,
+		removedLinesCount: number,
+	): void {
+		searchResults.applyLinesDelta(firstLineNumber, removedLinesCount);
+		this.updateLinesSearchResults(document, searchResults, firstLineNumber, removedLinesCount);
+	}
+
+	private _searchInLine(regexp: RegExp, text: string, lineNumber: number): Range[] {
+		const matches: Range[] = [];
+		const m = text.matchAll(regexp);
+		for (const match of m) {
+			if (match.index === undefined) {
+				break;
+			}
+			matches.push(
+				new Range(lineNumber, match.index, lineNumber, match.index + match[0].length),
+			);
+		}
+		return matches;
+	}
+}
