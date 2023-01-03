895911e76b436e5e25f45560a83b3705d3ac1496
stuxbart <stuxbart99@gmail.com>
2023-01-03 18:48:51 +0100

    refactor: rename regexp search file

--- src/search/index.ts
+++ src/search/index.ts
@@ -1,6 +1,6 @@
 import SerachResults from './search-results';
 import { Search } from './search';
 import NaiveSearch from './naive-search';
-import RegExpSearch from './regex-search';
+import RegExpSearch from './regexp-search';
 
 export { Search, SerachResults, NaiveSearch, RegExpSearch };
